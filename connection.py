ebd338e2856de6556043a9f8a16088a5d8e2c9d3
juank218 <juancaquines@gmail.com>
2024-08-28 12:31:09 -0500

    EDA, csv clean and connection to DB

--- connection.py
+++ connection.py
@@ -5,23 +5,24 @@ import os
 
 load_dotenv()
 
-localhost = os.getenv('LOCALHOST')
+localhost = os.getenv('DB_HOST')
 port = os.getenv('PORT')
 nameDB = os.getenv('DB_NAME')
 userDB = os.getenv('DB_USER')
 passDB = os.getenv('DB_PASS')
 
-location_file = './Data csv/cars_raw.csv'  
-raw_table_database = 'cars'  
+
+location_file = 'Data/cars_clean.csv'  
+clean_table_database = 'cars'  
 
 engine = create_engine(f'postgresql+psycopg2://{userDB}:{passDB}@{localhost}:{port}/{nameDB}')
 
 try:
     df = pd.read_csv(location_file, sep=",")
 
-    df.to_sql(raw_table_database, engine, if_exists='replace', index=False)
+    df.to_sql(clean_table_database, engine, if_exists='replace', index=False)
 
-    print(f"Tabla '{raw_table_database}' creada y datos subidos exitosamente.")
+    print(f"Tabla '{clean_table_database}' creada y datos subidos exitosamente.")
 
 except Exception as e:
     print(f"Error al subir los datos: {e}")
